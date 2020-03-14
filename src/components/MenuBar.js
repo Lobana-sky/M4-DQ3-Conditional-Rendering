55e3e9ce94ae020757eb2f0c7b6d8f5e79bb360a
lobana-sky <loubanaeng@gmail.com>
2020-03-14 13:04:53 +0300

    id

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -15,19 +15,20 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className={props.whatSelect===0?"item active":"item"} id="profile" onClick={()=>props.handelClick(0)} >
+      <a className={props.whatSelect==="profile"?"item active":"item"} id="profile"
+       onClick={(e)=>props.handelClick(e.targer.id)} >
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className={props.whatSelect===1?"item active":"item"} id="photo" onClick={()=>props.handelClick(1)} >
+      <a className={props.whatSelect==="photo"?"item active":"item"} id="photo" onClick={(e)=>props.handelClick(e.targer.id)} >
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className={props.whatSelect===2?"item active":"item"} id="cocktail" onClick={()=>props.handelClick(2)}>
+      <a className={props.whatSelect==="cocktail"?"item active":"item"} id="cocktail" onClick={(e)=>props.handelClick(e.targer.id)}>
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className={props.whatSelect===3?"item active":"item"} id="pokemon" onClick={()=>props.handelClick(3)}> 
+      <a className={props.whatSelect==="pokemon"?"item active":"item"} id="pokemon" onClick={(e)=>props.handelClick(e.targer.id)}> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -6,7 +6,7 @@ class MainBox extends React.Component {
 constructor(){
 super();
 this.state={
-  select:0
+  select:"pokemon"
 }
 }
 handelClick = (selected)=>{
@@ -16,26 +16,23 @@ handelClick = (selected)=>{
 }
   render() {
     /*
-
     Replace the code below! Depending on what menu item is selected in the menu, 
     I should render either a Profile, Photos, Cocktails, or Pokemon component.
     Think of a way to track which menu item was selected. Which component should 
     have state? Which component should have methods to control state? Where should 
     these methods be called?
-
     */
-
     const detailsToDisplay =(state)=> {
-      if(state===0){
+      if(state==="profile"){
         return(<Profile />)
       }
-      else if(state===1){
+      else if(state==="photos"){
         return(<Photos />)
       }
-      else if(state===2){
+      else if(state==="cocktails"){
         return(<Cocktails />)
       }
-      else if(state===3){
+      else if(state==="pokemon"){
         return(<Pokemon />)
       }
     }
